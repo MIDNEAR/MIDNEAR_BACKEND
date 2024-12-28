8f929ad12ec537ef82d020b1e96391294f6b16d9
mjkang4416 <mjkang4416@naver.com>
2024-12-28 14:20:55 +0900

    [Build] 기초세팅

---
+++ src/main/java/com/midnear/midnearshopping/MidnearShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.midnear.midnearshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidnearShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidnearShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/midnear/midnearshopping/MidnearShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midnear.midnearshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidnearShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
