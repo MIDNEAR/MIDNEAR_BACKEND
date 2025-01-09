28e9bc6a63015f64a4d8b3c1e6f3f7fdfd41103c
mjkang4416 <mjkang4416@naver.com>
2025-01-09 17:33:47 +0900

    [Chore] 시큐리티
    엔드포인트 추가

--- src/main/java/com/midnear/midnearshopping/config/SecurityConfig.java
+++ src/main/java/com/midnear/midnearshopping/config/SecurityConfig.java
@@ -23,7 +23,8 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
             "/api/v1/member/**", "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
-            "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html", "/api/v1/auth/**","member/signup", "member/login", "disruptive/**","/sms/**"
+            "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html", "/api/v1/auth/**","member/signup", "member/login", "disruptive/**","/sms/**","/inquirie/**"
+            ,"/magazine/**"
     }; //더 열어둘 엔드포인트 여기에 추가
 
     @Bean
