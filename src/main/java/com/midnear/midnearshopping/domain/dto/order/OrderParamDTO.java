385279d92c5cdb05e4c0c053cc0c560dd7632826
mjkang4416 <mjkang4416@naver.com>
2025-01-14 19:36:55 +0900

    [Fix] 필터링 검색 기능 수정
    상태만 선택시 오류나는 부분 수정 and >= 조건
    내부로 이동
    
    Ref: #24

--- src/main/java/com/midnear/midnearshopping/domain/dto/order/OrderParamDTO.java
+++ src/main/java/com/midnear/midnearshopping/domain/dto/order/OrderParamDTO.java
@@ -1,12 +1,7 @@
 package com.midnear.midnearshopping.domain.dto.order;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import io.micrometer.common.lang.Nullable;
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Null;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.time.LocalDateTime;
 
 @Getter
