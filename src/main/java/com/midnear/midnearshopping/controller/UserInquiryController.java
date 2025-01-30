ba70ec02c13d6a1b9d46ca9b24e9eb430155a7ba
s0obang <tnqls40168@gmail.com>
2025-01-30 12:12:29 +0900

    [Feat] 문의 조회#38

--- src/main/java/com/midnear/midnearshopping/controller/UserInquiryController.java
+++ src/main/java/com/midnear/midnearshopping/controller/UserInquiryController.java
@@ -1,6 +1,8 @@
 package com.midnear.midnearshopping.controller;
 
+import com.midnear.midnearshopping.domain.dto.Inquiries.InquiriesDTO;
 import com.midnear.midnearshopping.domain.dto.Inquiries.InquiryRequestDto;
+import com.midnear.midnearshopping.domain.dto.Inquiries.UserInquiryListDto;
 import com.midnear.midnearshopping.domain.vo.users.CustomUserDetails;
 import com.midnear.midnearshopping.exception.ApiResponse;
 import com.midnear.midnearshopping.service.inquirie.UserInquiryService;
@@ -11,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/userInquiries")
@@ -29,6 +33,29 @@ public class UserInquiryController {
             log.error("문의글 작성 실패 - 유저 없음: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ApiResponse(false, "존재하지 않는 유저입니다.", null));
+        } catch (Exception e) {
+            log.error("문의글 작성 실패: {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse(false, "문의글 작성 중 오류가 발생했습니다.", null));
+        }
+    }
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getList(@RequestParam(value = "pageNumber", defaultValue = "1") int pageNumber) {
+        try {
+            List<UserInquiryListDto> response = userInquiryService.selectInquirylist(pageNumber);
+            return ResponseEntity.ok(new ApiResponse(true, "문의글 리스트 성공적 조회", response));
+
+        } catch (Exception e) {
+            log.error("문의글 작성 실패: {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse(false, "문의글 작성 중 오류가 발생했습니다.", null));
+        }
+    }
+    @GetMapping("/getInquiry")
+    public ResponseEntity<ApiResponse> getInquiry(@RequestParam Long inquiryId) {
+        try {
+            InquiriesDTO response = userInquiryService.selectInquiry(inquiryId);
+            return ResponseEntity.ok(new ApiResponse(true, "문의글 성공적 조회", response));
         } catch (Exception e) {
             log.error("문의글 작성 실패: {}", e.getMessage(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
--- src/main/java/com/midnear/midnearshopping/service/inquirie/UserInquiryService.java
+++ src/main/java/com/midnear/midnearshopping/service/inquirie/UserInquiryService.java
@@ -1,6 +1,9 @@
 package com.midnear.midnearshopping.service.inquirie;
 
+import com.midnear.midnearshopping.domain.dto.Inquiries.InquiriesDTO;
+import com.midnear.midnearshopping.domain.dto.Inquiries.InquiriesListDTO;
 import com.midnear.midnearshopping.domain.dto.Inquiries.InquiryRequestDto;
+import com.midnear.midnearshopping.domain.dto.Inquiries.UserInquiryListDto;
 import com.midnear.midnearshopping.domain.vo.Inquiries.InquiriesVO;
 import com.midnear.midnearshopping.domain.vo.claim.CanceledProductVO;
 import com.midnear.midnearshopping.mapper.Inquiries.UserInquiryMapper;
@@ -8,13 +11,18 @@ import com.midnear.midnearshopping.mapper.users.UsersMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserInquiryService {
     private UserInquiryMapper userInquiryMapper;
     private UsersMapper usersMapper;
+    private static final int pageSize = 2;
 
+    @Transactional
     public void createInquiry(String id, InquiryRequestDto requestDto) {
         Integer userId =  usersMapper.getUserIdById(id);
         if(userId == null) {
@@ -26,5 +34,26 @@ public class UserInquiryService {
             throw new RuntimeException("문의글 작성중 오류 발생");
         }
     }
+    @Transactional
+    public List<UserInquiryListDto> selectInquirylist(int pageNumber) {
+        try {
+            int offset = (pageNumber - 1) * pageSize;
+            return userInquiryMapper.SelectInquirylist(offset, pageSize);
+        } catch (Exception e) {
+            throw new RuntimeException("문의글 리스트 조회중 오류 발생");
+        }
+    }
+
+    @Transactional
+    public InquiriesDTO selectInquiry(Long inquiryId) {
+        try {
+            userInquiryMapper.updateViewCount(inquiryId);
+            return userInquiryMapper.selectInquiries(inquiryId);
+        } catch (Exception e) {
+            throw new RuntimeException("문의글 조회중 오류 발생");
+        }
+    }
+
+
 
 }
