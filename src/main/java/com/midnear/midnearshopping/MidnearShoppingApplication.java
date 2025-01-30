e36d4a71096ac7d65b93492043b9bebff8ad4476
mjkang4416 <mjkang4416@naver.com>
2025-01-31 05:00:04 +0900

    [fix] 배송정보 업데이트
    관리자 페이지 모든 기본 배송정보 자동 업데이트

--- src/main/java/com/midnear/midnearshopping/MidnearShoppingApplication.java
+++ src/main/java/com/midnear/midnearshopping/MidnearShoppingApplication.java
@@ -2,8 +2,10 @@ package com.midnear.midnearshopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MidnearShoppingApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/midnear/midnearshopping/service/productManagement/DeliveryServiceImpl.java
@@ -0,0 +1,77 @@
+package com.midnear.midnearshopping.service.productManagement;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.midnear.midnearshopping.domain.dto.delivery.DeliveryInfoDTO;
+import com.midnear.midnearshopping.mapper.productManagement.ShippingManagementMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DeliveryServiceImpl implements DeliveryService {
+
+    private final ShippingManagementMapper shippingManagementMapper;
+    private final RestTemplate restTemplate;
+    private static final String SMART_TRACKING_API_URL = "http://info.sweettracker.co.kr/api/v1/trackingInfo";
+
+    @Scheduled(fixedRate = 3600000)
+    @Override
+    public void updateTrackingStatus() {
+        List<DeliveryInfoDTO> inTransitOrders = shippingManagementMapper.getInTransitOrders();
+
+        for (DeliveryInfoDTO deliveryinfo : inTransitOrders) {
+            String url = SMART_TRACKING_API_URL + "?t_code=" + deliveryinfo.getCarrierCode() + "&t_invoice=" + deliveryinfo.getInvoiceNumber() + "&t_key=" + "P4pBDvEWDxfhFhnnNRs9NA";
+
+            try {
+                ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+
+                if (response.getStatusCode() == HttpStatus.OK) {
+                    String status = parseStatusFromResponse(response.getBody());
+
+                    if ("배송완료".equals(status)) {  // 스마트택배 API에서 "배송완료" 확인
+                        try {
+                            shippingManagementMapper.updateTrackingStatus(deliveryinfo.getDeliveryId());
+                            log.info("배송완료 업데이트 성공");
+                        } catch (Exception e) {
+                            log.error("배송완료 업데이트 실패",e);
+                        }
+                    }
+                } else {
+                    log.warn("스마트택배 API 응답 오류");
+                }
+            } catch (Exception e) {
+                log.error("스마트택배 API 호출 실패 : {}", e.getMessage(), e);
+            }
+        }
+
+        }
+    @Override
+    public String parseStatusFromResponse(String responseBody) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+            if (jsonNode.has("lastStateDetail") && jsonNode.get("lastStateDetail").has("kind")) {
+                return jsonNode.get("lastStateDetail").get("kind").asText();
+            } else {
+                log.warn("응답 내용: {}", responseBody);
+                return "unknown";  // lastStateDetail이 없는 경우
+            }
+        } catch (Exception e) {
+            log.error("JSON 파싱 오류: {}", e.getMessage(), e);
+            return "unknown";
+        }
+    }
+}
+
+
