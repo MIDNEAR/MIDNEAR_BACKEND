b5d799e7af7e18d0c0f9461648e2d4b66fa80505
s0obang <tnqls40168@gmail.com>
2025-02-01 02:44:36 +0900

    [Feat] 포인트, 쿠폰 관리..#38

--- src/main/java/com/midnear/midnearshopping/domain/vo/coupon_point/UserCouponVO.java
+++ src/main/java/com/midnear/midnearshopping/domain/vo/coupon_point/UserCouponVO.java
@@ -1,6 +1,14 @@
 package com.midnear.midnearshopping.domain.vo.coupon_point;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserCouponVO {
     Long userCouponId;
     boolean usedStatus;
+    Integer userId;
 }
