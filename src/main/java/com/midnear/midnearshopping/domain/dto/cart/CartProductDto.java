68d57a704da8887040688e0d71e38336faa82906
s0obang <tnqls40168@gmail.com>
2025-02-10 16:55:20 +0900

    [Fix] 장바구니 상품 수정

--- src/main/java/com/midnear/midnearshopping/domain/dto/cart/CartProductDto.java
+++ src/main/java/com/midnear/midnearshopping/domain/dto/cart/CartProductDto.java
@@ -3,6 +3,9 @@ package com.midnear.midnearshopping.domain.dto.cart;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+
 @Getter
 @Setter
 public class CartProductDto {
@@ -15,7 +18,12 @@ public class CartProductDto {
     //여기까지는 cart_product테이블에서 가져와야하고
     //여기서부터
     private String productName;
-    private String price;
+    private BigDecimal price;
+    private Integer discountRate;
+    private BigDecimal discountPrice;
+    private Date discountStartDate;
+    private Date discountEndDate;
     //여기까지는 product 테이블에서 가져와야
     private String productImage; // 얘는 product_images테이블에서 가져와야하고
+    private String color;
 }
