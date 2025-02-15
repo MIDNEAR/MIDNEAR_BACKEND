0e4eece86f25a229bce815e6d00759facdcee795
mjkang4416 <mjkang4416@naver.com>
2025-02-15 18:38:02 +0900

    [Feat] 무료, 기본 배송비 띄우기
    무료, 기본배송비 띄우는 api 추가
    
    Related to: #58

--- src/main/java/com/midnear/midnearshopping/controller/productManage/ShippingManageController.java
+++ src/main/java/com/midnear/midnearshopping/controller/productManage/ShippingManageController.java
@@ -1,5 +1,6 @@
 package com.midnear.midnearshopping.controller.productManage;
 
+import com.midnear.midnearshopping.domain.dto.delivery.FreeBasicConditionDTO;
 import com.midnear.midnearshopping.domain.dto.productManagement.ShippingManageDTO;
 import com.midnear.midnearshopping.exception.ApiResponse;
 import com.midnear.midnearshopping.service.productManagement.ShippingManageService;
@@ -35,10 +36,26 @@ public class ShippingManageController {
     @GetMapping("/selectShippingFee")
     public ResponseEntity<ApiResponse> selectShippingFee() {
         try {
-            shippingManageService.selectShippingFee();
+            ShippingManageDTO shippingManageDTO = shippingManageService.selectShippingFee();
             // 200 OK 응답으로 JSON 반환
             return ResponseEntity.status(HttpStatus.OK)
-                    .body(new ApiResponse(true, "성공적으로 가져왔습니다.", null));
+                    .body(new ApiResponse(true, "성공적으로 가져왔습니다.", shippingManageDTO));
+
+            // 200 OK 응답으로 JSON 반환
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse(false, "서버 오류가 발생했습니다.", null));
+        }
+
+    }
+
+    @GetMapping("/selectFreeBasicFee")
+    public ResponseEntity<ApiResponse> FreeBasicFee() {
+        try {
+            FreeBasicConditionDTO freeBasicConditionDTO = shippingManageService.selectFreeBasicFee();
+            // 200 OK 응답으로 JSON 반환
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ApiResponse(true, "성공적으로 가져왔습니다.", freeBasicConditionDTO));
 
             // 200 OK 응답으로 JSON 반환
         } catch (Exception ex) {
